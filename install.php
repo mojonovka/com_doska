f59511863d058bdb6ae4ca867c8c3134ca9891fd
mojonovka <mojonovka@gmail.com>
2025-03-07 15:13:56 +0100

    _06_script_

--- install.php
+++ install.php
@@ -1,6 +1,35 @@
 <?php
 
+defined('_JEXEC') or die('');
 
+class com_DoskaInstallerScript {
+
+    public function install($parent){
+        echo "-->install extention";
+        echo JText::_('COM_DOSKA_INSTALL_STRING');
+    }
+
+    public function update($parent){
+        echo "-->update extention";
+        echo JText::_('COM_DOSKA_UPDATE_STRING');
+    }
+
+    public function uninstall($parent){
+        echo "-->uninstall extention";
+        echo JText::_('COM_DOSKA_UNINSTALL_STRING');
+    }
+
+    public function preflight($type, $parent){
+        echo "-->update extention";
+        echo JText::_('COM_DOSKA_PRE_STRING');
+    }
+
+    public function postflight($type, $parent){
+        echo "-->update extention";
+        echo JText::_('COM_DOSKA_POST_STRING');
+    }
+
+}
 
 
 ?>
