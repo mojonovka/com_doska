9e46f99ec5ca33068db7ccc104f53d38ca7cd014
mojonovka <mojonovka@gmail.com>
2025-03-08 20:08:29 +0100

    _09_end_toolbar_

--- admin/views/types/view.html.php
+++ admin/views/types/view.html.php
@@ -7,6 +7,7 @@ class DoskaViewTypes extends jViewLegacy {
     public function display($tpl = null){
 
         $this->addToolBar();
+        $this->setDocument();
 
         parent::display($tpl);
 
@@ -14,14 +15,33 @@ class DoskaViewTypes extends jViewLegacy {
 
     protected function addToolBar(){
 
-        JToolbarHelper::title(JText::_("COM_DOSKA_MANEGER_TYPES"));
+        JToolbarHelper::title(JText::_("COM_DOSKA_MANEGER_TYPES"), 'doskatitle');
         JToolbarHelper::addNew('type.add', JText::_('COM_DOSKA_MANEGER_TYPES_ADD'));
         JToolbarHelper::editList('type.edit');
+        JToolbarHelper::deleteList(JText::_("COM_DOSKA_MANEGER_TYPES_DELETE_MSG"), 'types.delete');
         JToolbarHelper::divider();        
         JToolbarHelper::publish('types.publish', 'JTOOLBAR_PUBLISH', TRUE);
         JToolbarHelper::unpublish('types.unpublish', 'JTOOLBAR_UNPUBLISH', TRUE);
-        JToolbarHelper::divider();        
+        JToolbarHelper::divider();    
+
+        JToolbatHelper::custom('type.create', 'doskabutton', 'doskabutton_hover', JText::_('COM_DOSKA_MANEGER_TYPES_CUSTOM'), false);
+        JToolbarHelper::editHtml();
+        JToolbarHelper::editCss();
         JToolbarHelper::preferences('com_doska');
+
+        echo JUri::root()."<br />";
+        echo JUri::base()."<br />";
+        echo JUri::current()."<br />";
+
+        print_r(JUri::getInstance()->getVar('view'));
+ 
+    }
+
+    protected function setDocument(){
+
+        $document = JFactory::getDocument();
+        $document->addStyleSheet(JUri::root(true)."/media/com_doska/css/style.css");
+
 
     }
 
