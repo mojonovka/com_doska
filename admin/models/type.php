516ea847f08288a7588a34404f3b967c8ddbee09
mojonovka <mojonovka@gmail.com>
2025-03-09 21:11:04 +0100

    _11_add_model_

---
+++ admin/models/type.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('_JEXEC') or die();
+
+class DoskaModelType extends JModelAdmin {
+
+    public function getForm($data = array(), $loadData = true){
+
+        $form = array (1,2,3,4);
+
+
+        return $form;
+
+    }
+
+
+}
+
+
+
+
+?>
--- admin/views/type/view.html.php
+++ admin/views/type/view.html.php
@@ -4,7 +4,46 @@ defined("_JEXEC") or die();
 
 class DoskaViewType extends jViewLegacy {
 
-    
+    protected $form;
+
+    public function display($tmpl = null){
+
+        $this->form = $this->get('Form');//getForm
+
+        $this->addToolBar();
+        $this->setDocument();
+
+        parent::display($tmpl);
+
+    }
+
+    protected function addToolBar(){
+        
+        $isnew = ($this->item->id == 0);
+
+        if($isnew){
+            $title = _JText::_('COM_DOSKA_MANAGER_TYPE_NEW_MANEGER_TITLE');
+        } else {
+            $title = JText::_('COM_DOSKA_MANAGER_TYPE_EDIT_MANEGER_TITLE');
+        }
+
+        JToolbarHelper::title($title);
+        JToolbarHelper::apply('type.apply');
+        JToolbarHelper::save('type.save');
+        JToolbarHelper::cancel('type.cancel');
+
+    }
+
+    protected function setDocument(){
+
+        $document = JFactory::getDocument();
+        $document->setTitle(JText::_('COM_DOSKA_MANAGER_TYPE_NEW_PAGE_TITLE'));
+        $document->addStyleSheet(JUri::root(true).'/media/com_doska/css/style.css');
+
+
+
+
+    }
 
 }
 
